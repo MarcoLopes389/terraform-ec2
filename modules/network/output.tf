e254b681c61d76e1ae6a7990e67913d5549ec55a
Marco <marco.lopes.araujo@gmail.com>
2025-12-19 16:03:34 -0300

    feat: set network module output

---
+++ modules/network/output.tf
@@ -0,0 +1,11 @@
+output "subnet_pub_1a" {
+  value = aws_subnet.estudos_subnet_pub_1a.id
+}
+
+output "subnet_pub_1b" {
+  value = aws_subnet.estudos_subnet_pub_1b.id
+}
+
+output "vpc" {
+  value = aws_vpc.estudos_vpc.id
+}
