99957282fba85fdf8b092d15c44c91d2344a86f8
Marco <marco.lopes.araujo@gmail.com>
2025-12-19 15:04:55 -0300

    feat: added variables and data sources

--- public.tf
+++ public.tf
@@ -1,6 +1,7 @@
 resource "aws_subnet" "estudos_subnet_pub_1a" {
   vpc_id                  = aws_vpc.estudos_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.estudos_vpc.cidr_block, 8, 1)
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
+  availability_zone       = "${data.aws_region.current}a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -10,7 +11,8 @@ resource "aws_subnet" "estudos_subnet_pub_1a" {
 
 resource "aws_subnet" "estudos_subnet_pub_1b" {
   vpc_id                  = aws_vpc.estudos_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.estudos_vpc.cidr_block, 8, 2)
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
+  availability_zone       = "${data.aws_region.current}b"
   map_public_ip_on_launch = true
 
   tags = {
